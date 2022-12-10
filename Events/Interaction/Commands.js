0beec647452a5c31189835b5fa11450268ba7d2e
Zach <24686542+ZachZeop@users.noreply.github.com>
2022-12-09 21:09:07 -0500

    added missing ]

--- Events/Interaction/Commands.js
+++ Events/Interaction/Commands.js
@@ -20,11 +20,11 @@ module.exports = {
         
         const command = client.commands.get(commandName)
 
-        if (!command) return interaction.reply({ embeds:[embed.setDescription(`An error occurred while running the command!`), ephemeral: true}) && client.commands.delete(commandName)
+        if (!command) return interaction.reply({ embeds:[embed.setDescription(`An error occurred while running the command!`)], ephemeral: true}) && client.commands.delete(commandName)
 
-        if (command.UserPerms && command.UserPerms.length !== 0) if (!member.permissions.has(command.UserPerms)) return interaction.reply({ embeds:[embed.setDescription(`You need \`${command.UserPerms.join(", ")}\` permission(s) to execute this command!`), ephemeral: true });
+        if (command.UserPerms && command.UserPerms.length !== 0) if (!member.permissions.has(command.UserPerms)) return interaction.reply({ embeds:[embed.setDescription(`You need \`${command.UserPerms.join(", ")}\` permission(s) to execute this command!`)], ephemeral: true });
 
-        if (command.BotPerms && command.BotPerms.length !== 0) if (!guild.members.me.permissions.has(command.BotPerms)) return interaction.reply({ embeds:[embed.setDescription(`I need \`${command.BotPerms.join(", ")}\` permission(s) to execute this command!`), ephemeral: true});
+        if (command.BotPerms && command.BotPerms.length !== 0) if (!guild.members.me.permissions.has(command.BotPerms)) return interaction.reply({ embeds:[embed.setDescription(`I need \`${command.BotPerms.join(", ")}\` permission(s) to execute this command!`)], ephemeral: true});
 
         command.execute(interaction, client)
 
